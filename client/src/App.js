af8905023f3db0d3b485d690006a8c2175cca32a
Quan Nguyen <s4622573@student.uq.edu.au>
2021-02-07 22:49:39 +0700

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,44 @@
 import './App.css';
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <Router>
-      <Fragment>
-        <Navbar/>
-        <Route exact path='/' component={Landing}/>
-        <section>
-          <Switch>
-            <Route exact path='/register' component={Register}/>
-            <Route exact path='/login' component={Login}/>
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar/>
+          <Route exact path='/' component={Landing}/>
+          <section>
+            <Alert/>
+            <Switch>
+              <Route exact path='/register' component={Register}/>
+              <Route exact path='/login' component={Login}/>
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,10 +21,15 @@ const Register = () => {
     e.preventDefault();
     e.stopPropagation();
     if (password !== confirmPassword) {
-      console.log('Not good');
+      setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('Good');
+      register({ name, email, password });
     }
+  }
+
+  // Redirect if signed up
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard'/>
   }
 
   return (
@@ -68,4 +77,14 @@ const Register = () => {
   )
 }
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
