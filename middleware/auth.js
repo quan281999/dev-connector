5739270e492dfbb1fe125a791a1d3a7ee0184edb
Quan Nguyen <quan281999@gmail.com>
2021-09-25 21:21:12 +0700

    refactored auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,21 +2,24 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function (req, res, next) {
-  // Get token from header
-  const token = req.header('x-auth-token');
-
-  // Check if not token
-  if (!token) {
+  // Check header
+  if (!req.header('Authorization')) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
+  } else {
+    const auth = req.header('Authorization').split(' ');
+    // Extract token
+    if (auth.length === 2 && /^Bearer$/i.test(auth[0])) {
+      const token = auth[1];
+      // Verify token
+      try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        next();
+      } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+      }
+    } else {
+      return res.status(401).json({ msg: 'Token is not valid' });
+    }
   }
-
-  // Verify token
-  try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
-
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
-  }
-}
+};
